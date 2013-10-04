5afc96cf451ca56e45faa32df98ffc211110cb48
Andy <andy@norborg.net>
2013-10-04 10:24:57 -0700

    Changed rcVerifySet to use rcSubmit instead of rcAttempt

--- src/directives/rcVerifySet.js
+++ src/directives/rcVerifySet.js
@@ -11,8 +11,8 @@
  *
  * @element ANY
  */
-var rcVerifySetDirective = {    'rcVerifySet': function () {        return {            restrict: 'A',            require: ['^rcAttempt', 'ngModel'],            link: function (scope, element, attributes, controllers) {                var attemptController = controllers[0];                var modelController = controllers[1];
+var rcVerifySetDirective = {    'rcVerifySet': function () {        return {            restrict: 'A',            require: ['^rcSubmit', 'ngModel'],            link: function (scope, element, attributes, controllers) {                var submitController = controllers[0];                var modelController = controllers[1];
                 
-                attemptController.onAttempt(function() {
+                submitController.onAttempt(function() {
                     modelController.$setViewValue(element.val());
                 });            }        };    }};
