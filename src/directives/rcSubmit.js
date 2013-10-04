6f6a901d90a9d757447e5c89d449edb4fc6e7f6b
Andy <andy@norborg.net>
2013-10-04 07:03:34 -0700

    collapsed rcAttempt into rcSubmit

--- src/directives/rcSubmit.js
+++ src/directives/rcSubmit.js
@@ -13,19 +13,72 @@ var rcSubmitDirective = {
     'rcSubmit': ['$parse', function ($parse) {
         return {
             restrict: 'A',
-            require: 'form',
-            link: function (scope, formElement, attributes, formController) {
+            require: ['rcSubmit', '?form'],
+            controller: ['$scope', function ($scope) {
+            	var formController = null;
             	
-                var fn = $parse(attributes.rcSubmit);
-
-                formElement.bind('submit', function (event) {
-                    // if form is not valid cancel it.
-                    if (!formController.$valid) return false;
-                    
-                    scope.$apply(function() {
-                        fn(scope, {$event:event});
+                this.attempted = false;
+                
+                var attemptHandlers = [];
+                
+                this.onAttempt = function(handler) {
+                    attemptHandlers.push(handler);
+                };
+                
+                this.setAttempted = function() {
+                    this.attempted = true;
+                  
+                    angular.forEach(attemptHandlers, function (handler) {
+                        handler();
                     });
-                });
+                };
+                
+                this.setFormController = function(controller) {
+                    formController = controller;
+                };
+                
+                this.needsAttention = function (fieldModelController) {
+                    if (!formController) return false;
+                    
+                    if (fieldModelController) {
+                        return fieldModelController.$invalid && (fieldModelController.$dirty || this.attempted);
+                    } else {
+                        return formController && formController.$invalid && (formController.$dirty || this.attempted);
+                    }
+                };
+            }],
+            compile: function(cElement, cAttributes, transclude) {
+                return {
+                    pre: function(scope, formElement, attributes, controllers) {
+                    	
+                    	var attemptController = controllers[0];
+                        var formController = (controllers.length > 1) ? controllers[1] : null;
+                        
+                        attemptController.setFormController(formController);
+                        
+                        scope.rc = scope.rc || {};
+                        scope.rc[attributes.name] = attemptController;
+                    },
+                    post: function(scope, formElement, attributes, controllers) {
+                    	
+                    	var attemptController = controllers[0];
+                    	var formController = (controllers.length > 1) ? controllers[1] : null;
+                    	
+                    	var fn = $parse(attributes.rcSubmit);
+                    	
+                        formElement.bind('submit', function () {
+                            attemptController.setAttempted();
+                            if (!scope.$$phase) scope.$apply();
+                            
+                            // if form is not valid cancel it.
+                            if (!formController.$valid) return false;
+                    
+                            scope.$apply(function() {
+                                fn(scope, {$event:event});
+                            });
+                        });
+                }
+              };
             }
         };
     }]
--- src/modules/rcForm.js
+++ src/modules/rcForm.js
@@ -5,4 +5,4 @@
  * @description
  * Module to encapsulate all of our custom form-based directives.
  */
-var rcFormModule = angular.module('rcForm', ['ng']);if (rcSubmitDirective) rcFormModule.directive(rcSubmitDirective);if (rcAttemptDirective) rcFormModule.directive(rcAttemptDirective);if (rcVerifySetDirective) rcFormModule.directive(rcVerifySetDirective);
+var rcFormModule = angular.module('rcForm', ['ng']);if (rcSubmitDirective) rcFormModule.directive(rcSubmitDirective);if (rcVerifySetDirective) rcFormModule.directive(rcVerifySetDirective);
