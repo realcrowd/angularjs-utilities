14729fb56cd4b65c16d98fa0e1656bd0ddc342d5
Andy <andy@norborg.net>
2013-10-08 07:29:39 -0700

    updated rcDisabled to have a separate module for Bootstrap

--- examples/login-with-disable/scripts/LoginApp.js
+++ examples/login-with-disable/scripts/LoginApp.js
@@ -1,18 +1,2 @@
 // define module for app
-angular.module('LoginApp', ['rcForm', 'rcDisabled'])
-.config(['rcDisabledProvider', function(rcDisabledProvider) {
-  rcDisabledProvider.onDisable(function(rootElement, isDisabled) {
-    var jElement = jQuery(rootElement);
-      
-    return jElement
-            .find(':not([rc-disabled])')
-            .filter(function(index) {
-              return jQuery(this).parents().not(jElement).filter('[rc-disabled]').length === 0;
-            })
-            .filter('input:not([ng-disabled]), button:not([ng-disabled]), .btn, li')
-            .add(jElement)
-            .toggleClass('disabled', isDisabled)
-            .filter('input, button')
-            .prop('disabled', isDisabled);
-  });
-}]);
+angular.module('LoginApp', ['rcForm', 'rcDisabledBootstrap']);
